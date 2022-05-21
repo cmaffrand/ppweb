bab18418bb3319910446f67f405582432c53a883
Carlos Maffrand <carlosmaffrand5@gmail.com>
2022-05-21 18:24:15 -0300

    Fixture DB is Added, and Prode DB dont have teams names anymore.

--- src/website/models.py
+++ src/website/models.py
@@ -12,11 +12,19 @@ class Note(db.Model):
 class Prode(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.DateTime(timezone=True), default=func.now())
+    gameid = db.Column(db.Integer)
     team1goals = db.Column(db.Integer)
     team2goals = db.Column(db.Integer)
-    teamadvance = db.Column(db.Integer)
+    teamadvance = db.Column(db.Integer, default=0)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+class Fixture(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    gameid = db.Column(db.Integer)
+    group = db.Column(db.String(150))
     team1 = db.Column(db.String(150))
-    team2 = db.Column(db.String(150))
+    team2 = db.Column(db.String(150))    
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class User(db.Model, UserMixin):
@@ -26,5 +34,6 @@ class User(db.Model, UserMixin):
     first_name = db.Column(db.String(150))
     notes = db.relationship('Note')
     prodes = db.relationship('Prode')
+    fixtures = db.relationship('Fixture')
     
     
