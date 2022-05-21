f67b7f0f3cdea5f7ffd87c9a581acbfd181a73ad
Carlos Maffrand <carlosmaffrand5@gmail.com>
2022-05-21 18:22:53 -0300

    Adds initilize db capabilities to the project.
    If the users are hardcoded, the fixture shuold be initialized for each player.

--- src/website/auth.py
+++ src/website/auth.py
@@ -1,19 +1,46 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from .models import User
+from .models import User, Fixture
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from flask_login import login_user, login_required, logout_user, current_user
 import os
+import csv
+from datetime import datetime
 
 IMG_FOLDER = os.path.join('static', 'img')
 filename_logo = os.path.join(IMG_FOLDER, 'layeta_inv.png')
 auth = Blueprint('auth', __name__)
+FIXTURE_PATH = 'website/static/info/fixture.csv'
+
+def init_db():
+    # Initialize database Users
+    new_user = User(email='carlosmaffrand5@gmail.com', first_name='Carlos', password=generate_password_hash(
+                'password1', method='sha256'))
+    db.session.add(new_user)
+    db.session.commit()    
+    
+    # Init DB Fixture
+    with open(FIXTURE_PATH) as fixture_csv:
+        file = csv.reader(fixture_csv, delimiter=',')
+        for row in file:
+            fixture = Fixture(gameid=int(row[0]), group=row[1], team1=row[2], team2=row[3], date=datetime.strptime(row[4],'%y/%m/%d %H:%M:%S'),user_id=1)
+            db.session.add(fixture)
+            db.session.commit()    
+    return True
 
 # Login page
 
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    
+    if not hasattr(login, "var_init_db"):
+        login.var_init_db  = False
+        
+    if not login.var_init_db:
+        init_db()
+        login.var_init_db = True
+        
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
