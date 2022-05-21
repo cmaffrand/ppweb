b0acc93a65c4c8ce246db36d9fe4ee83abdb5b4d
Carlos Maffrand <carlosmaffrand5@gmail.com>
2022-05-21 18:26:45 -0300

    Adds fixture and pronostics funtionalities.

--- src/website/views.py
+++ src/website/views.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
-from .models import Note
+from .models import Note, User, Prode, Fixture
 from . import db
 import json
 import os
+from werkzeug.security import generate_password_hash
+import datetime
 
 IMG_FOLDER = os.path.join('static', 'img')
 filename_logo = os.path.join(IMG_FOLDER, 'layeta_inv.png')
@@ -15,6 +17,7 @@ views = Blueprint('views', __name__)
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
+    
     if request.method == 'POST':
         note = request.form.get('note')
 
@@ -50,13 +53,30 @@ def delete_note():
 # User Pronostic webpage
 
 
-@views.route('/pronostics')
+@views.route('/pronostics', methods=['GET', 'POST'])
 @login_required
 def pronostics():
+    
+    if request.method == 'POST':
+        team1goals = request.form.get('team1goals')
+        team2goals = request.form.get('team2goals')
+        gameid = request.form.get('gameid')
+
+        new_prode = Prode(team1goals=team1goals, team2goals=team2goals, gameid=gameid, user_id=current_user.id)
+        db.session.add(new_prode)
+        db.session.commit()
+        flash('Pronostic Added!', category='success')
+        
+    print(datetime.datetime.utcnow()-datetime.timedelta(hours=3))
+            
     return render_template("pronostics.html",
                            user=current_user,
                            logo_image=filename_logo,
-                           username=current_user.first_name)
+                           username=current_user.first_name,
+                           time=datetime.datetime.utcnow()-datetime.timedelta(hours=3))
+                           #time=datetime.datetime(2022, 11, 25, 10, 1, 0)) ## Out of date test 
+    
+    ## Out of date test time=datetime(2022, 11, 25, 11, 0, 0)))
 
 # General Result and Pronostic webpage
 
@@ -65,6 +85,16 @@ def pronostics():
 @login_required
 def results():
     return render_template("results.html",
+                           user=current_user,
+                           logo_image=filename_logo,
+                           username=current_user.first_name)
+    
+# Fixture webpage
+
+@views.route('/fixture')
+@login_required
+def fixture():
+    return render_template("fixture.html",
                            user=current_user,
                            logo_image=filename_logo,
                            username=current_user.first_name)
