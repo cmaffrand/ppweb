6393855503fbf1e888576c56c2a54f0580bff4cd
Carlos Maffrand <carlosmaffrand5@gmail.com>
2022-10-23 09:04:08 -0300

    Elimina carrusel de las paginas, se elimina las notas, se elimina popups, se pasa a resultados como main, se elimina home, se crea una funcion para extraer los reultados en tiempo real, se publica los resultados en tiempo real en la vista resultados

--- src/website/__init__.py
+++ src/website/__init__.py
@@ -20,7 +20,7 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User, Note, Fixture, Prode
+    from .models import User, Fixture, Prode
 
     create_database(app)
 
